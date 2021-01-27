dbf8479d05e647718182d3d9cc19973233b83a7b
Francis Bonacic <francisbonacic@icloud.com>
2021-01-27 14:32:48 -0800

    setting up UpdateMovie Route

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UpdateMovie = () => {
+
+
+
+    return(
+        <form>
+            <input placeholder="movie title" /><br/>
+            <input placeholder="director" /><br/>
+            <input placeholder="metascore" /><br/>
+            <input placeholder="stars" /><br/>
+            <br/>
+            <button>Update</button>
+        </form>
+    )
+}
+
+export default UpdateMovie;
